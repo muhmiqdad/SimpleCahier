236594693d5506a3a7f372c5342dd537da6645bb
Muhammad Miqdad <32417850+muhmiqdad@users.noreply.github.com>
2021-08-08 18:24:24 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
 
-class App extends React.Component() {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isToggleOn: true,
-      printers: ['Iam'],
-      image: '',
-    };
-    this.handleClick = this.handleClick.bind(this);
-    this.printButton = this.printButton.bind(this);
-  }
+class App extends React.Component {
   render() {
     return (
       <React.Fragment>
-      <button onClick={this.printButton}>This is ac{this.state.isToggleOn ? 'ON' : 'OFF'} paragraph.</button>
-      <button onClick={this.handleClick}>another{this.state.isToggleOn ? 'ON' : 'OFF'} paragraph.</button>
-      <p>{this.state.printers}</p>
-      <img onLoad={this.load}  id="scream" src="icon2.png" alt="The Scream"/>
+        <p class="text-9xl">Hello world</p>
       </React.Fragment>
     );
+  }
 }
 
 export default App;
