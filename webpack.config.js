f5ad62514c5455b4f4c2ee309daebd1f34fa60c4
Muhammad Miqdad <32417850+muhmiqdad@users.noreply.github.com>
2021-08-08 12:13:07 +0700

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  output: {
+    path: path.resolve('build'),
+    filename: 'index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react']
+          }
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: ['tailwindcss', 'autoprefixer', ]
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: './build',
+    disableHostCheck: true,
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.join(__dirname, 'build', 'index.html'),
+    }),
+  ],
+};
